68091d999c8ca781ba2be37b85f336f418592afa
unknown <beatrizcristina08@hotmail.com>
2022-11-17 22:30:33 -0300

    second

--- src/common/types.js
+++ src/common/types.js
@@ -1,15 +1,4 @@
-  const cpf = {
-    type: 'string',
-    pattern: '^\\d{11}$',
-    example: '21554495008',
-  }
-  
-  const cnpj = {
-    type: 'string',
-    pattern: '^\\d{14}$',
-    example: '33400689000109',
-  }
-  
+
   const classesDeConsumo = [
     'residencial',
     'industrial',
@@ -19,8 +8,6 @@
   const modalidadesTarifarias = ['branca', 'convencional']
   
   module.exports = {
-    cpf,
-    cnpj,
     classesDeConsumo,
     modalidadesTarifarias,
   }
